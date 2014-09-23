e12f5807f04434029f62f3348398377b126fae71
manish <manishkongari@outlook.com>
2014-09-24 02:03:32 +0530

    ss

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -58,7 +58,8 @@ function statusChangeCallback(response) {
     } else if (response.status === 'not_authorized') {
       // The person is logged into Facebook, but not your app.
       document.getElementById('status').innerHTML = 'Please log ' +
-        'into this app.&nbsp;&nbsp;&nbsp;<fb:login-button data-size="large" scope="public_profile,email,user_friends" onlogin="checkLoginState();">Get Started</fb:login-button>';
+        'into this app.&nbsp;&nbsp;&nbsp;';
+        document.getElementById("demo").innerHTML ='<fb:login-button data-size="large" scope="public_profile,email,user_friends" onlogin="checkLoginState();">Get Started</fb:login-button>';
 
     } else {
       // The person is not logged into Facebook, so we're not sure if
