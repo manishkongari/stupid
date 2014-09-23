f0ca79ba39e9056ccd38cf89310e4449f1b9cb28
manish <manishkongari@outlook.com>
2014-09-24 01:56:15 +0530

    ss

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,29 +15,6 @@
 //= require turbolinks
 //= require_tree .
 
-function statusChangeCallback(response) {
-    console.log('statusChangeCallback');
-    console.log(response);
-    // The response object is returned with a status field that lets the
-    // app know the current login status of the person.
-    // Full docs on the response object can be found in the documentation
-    // for FB.getLoginStatus().
-    if (response.status === 'connected') {
-      // Logged into your app and Facebook.
-      testAPI();
-    } else if (response.status === 'not_authorized') {
-      // The person is logged into Facebook, but not your app.
-      document.getElementById('status').innerHTML = 'Please log ' +
-        'into this app.&nbsp;&nbsp;&nbsp;';
-
-    } else {
-      // The person is not logged into Facebook, so we're not sure if
-      // they are logged into this app or not.
-      document.getElementById('status').innerHTML = 'Please log ' +
-        'into Facebook.';
-        $('div.hide').hide();
-    }
-  }
 
   // This function is called when someone finishes with the Login
   // Button.  See the onlogin handler attached to it in the sample
@@ -68,6 +45,29 @@ function statusChangeCallback(response) {
   //    your app or not.
   //
   // These three cases are handled in the callback function.
+function statusChangeCallback(response) {
+    console.log('statusChangeCallback');
+    console.log(response);
+    // The response object is returned with a status field that lets the
+    // app know the current login status of the person.
+    // Full docs on the response object can be found in the documentation
+    // for FB.getLoginStatus().
+    if (response.status === 'connected') {
+      // Logged into your app and Facebook.
+      testAPI();
+    } else if (response.status === 'not_authorized') {
+      // The person is logged into Facebook, but not your app.
+      document.getElementById('status').innerHTML = 'Please log ' +
+        'into this app.&nbsp;&nbsp;&nbsp;<fb:login-button data-size="large" scope="public_profile,email,user_friends" onlogin="checkLoginState();">Get Started</fb:login-button>';
+
+    } else {
+      // The person is not logged into Facebook, so we're not sure if
+      // they are logged into this app or not.
+      document.getElementById('status').innerHTML = 'Please log ' +
+        'into Facebook.';
+       
+    }
+  }
 
   FB.getLoginStatus(function(response) {
     statusChangeCallback(response);
@@ -92,6 +92,5 @@ function statusChangeCallback(response) {
       console.log('Successful login for: ' + response.name);
       document.getElementById('status').innerHTML =
         'Thanks for logging in, ' + response.name + '!';
-        $('div.hide').hide();
-    });
+        $('div.hide').hide(); });
   }
