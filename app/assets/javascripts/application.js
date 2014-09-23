ae9c036535e60f2261fc9061418fd79d52f35cc0
manish <manishkongari@outlook.com>
2014-09-23 22:37:01 +0530

    ss

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,27 +19,27 @@
     FB.init({
       appId      : '1465011740446158',
       xfbml      : true,
+      frictionlessRequests : true, // enable frictionless requests
       version    : 'v2.1'
     });
 
     // ADD ADDITIONAL FACEBOOK CODE HERE
   };
+(function(d){
+  var js, id = 'facebook-jssdk'; if (d.getElementById(id)) {return;}
+  js = d.createElement('script'); js.id = id; js.async = true;
+  js.src = "//connect.facebook.net/en_US/all.js";
+  d.getElementsByTagName('head')[0].appendChild(js);
+}(document));
 
-  (function(d, s, id){
-     var js, fjs = d.getElementsByTagName(s)[0];
-     if (d.getElementById(id)) {return;}
-     js = d.createElement(s); js.id = id;
-     js.src = "//connect.facebook.net/en_US/sdk.js";
-     fjs.parentNode.insertBefore(js, fjs);
-   }(document, 'script', 'facebook-jssdk'));
-  function onLogin(response) {
-  if (response.status == 'connected') {
-    FB.api('/me?fields=first_name', function(data) {
-      var welcomeBlock = document.getElementById('fb-welcome');
-      welcomeBlock.innerHTML = 'Hello, ' + data.first_name + '!';
-    });
-  }
-}
+
+
+
+
+
+
+
+
 
 FB.getLoginStatus(function(response) {
   // Check login status on load, and if the user is
