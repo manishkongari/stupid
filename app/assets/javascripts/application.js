96d666aa9291303a352b61e7124f5145a4a5af70
manish <manishkongari@outlook.com>
2014-09-24 13:57:18 +0530

    fb

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -107,7 +107,7 @@ function requestToFriends() {
   FB.ui({
       method: 'apprequests',
       title: 'View Source Request',
-      message: 'Join me and Find Out My Valentine From you!'
+      message: 'Join Me & Find Out My Valentine From You!'
   }); // Call to FB.ui
 } // messageToFriend
 
