327b2bd5b5b0f42929b859eac4d0de0fca0ebdf0
manish <manishkongari@outlook.com>
2014-09-27 20:44:35 +0530

    new heroku

--- app/models/fb.rb
+++ app/models/fb.rb
@@ -1,7 +1,3 @@
 class Fb < ActiveRecord::Base
-  validates_presence_of :userid
-  validates_presence_of :name
-  validates_presence_of :email
-  validates_presence_of :friend
-  validates_presence_of :hint
+
 end
